168f1496f174d5b9e4f8709b6f0adcdf86a50833
Facundo D'addese <facundo.daddese19@gmail.com>
2026-08-01 01:31:32 -0300

    responsive terminado

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,13 +5,19 @@ import { useLocation } from "react-router-dom";
 
 const thresholds = {
   "/": { desktop: 1400, w1024: 2700, w768: 3800, w600: 5000, w425: 9800 },
-  mccombos: { w1024: 2800, w768: 3550, w600: 5200, w425: 10300 },
-  hamburguesas: { w1024: 2000, w768: 2700, w600: 4000, w425: 7500 },
+  mccombos: { desktop: 1000, w1024: 2800, w768: 3550, w600: 5200, w425: 10300 },
+  hamburguesas: {
+    desktop: 1000,
+    w1024: 2000,
+    w768: 2700,
+    w600: 4000,
+    w425: 7500,
+  },
   "pollo-y-mcnuggets": { w1024: 710, w768: 1100, w600: 1500, w425: 2700 },
   "cajita-feliz": { w1024: 800, w768: 650, w600: 1000, w425: 8500 },
   "para-acompanar": { w1024: 1100, w768: 1100, w600: 1850, w425: 3500 },
   mcshakes: { w1024: 400, w768: 650, w600: 650, w425: 1500 },
-  postres: { w1024: 1500, w768: 1900, w600: 2800, w425: 5500 },
+  postres: { desktop: 550, w1024: 1500, w768: 1900, w600: 2800, w425: 5500 },
   ensaladas: { w1024: 420, w768: 400, w600: 350, w425: 280 },
   bebidas: { w1024: 750, w768: 1100, w600: 1500, w425: 2700 },
 };
@@ -19,6 +25,10 @@ const thresholds = {
 const Nav = () => {
   const [isSticky, setIsSticky] = useState(true);
   const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   useEffect(() => {
     const getKey = () => {
